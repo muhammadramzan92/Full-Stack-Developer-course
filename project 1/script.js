cf16b6f68cad02ce1a9d11221ec289462f790ddd
Muhammad Ramzan <61560701+muhammadramzan92@users.noreply.github.com>
2022-08-16 21:35:00 +0500

    project 2 html
    HTML Complete for project to movie screen

--- project 1/script.js
+++ project 1/script.js
@@ -4,11 +4,34 @@ const email=document.getElementById('email');
 const password=document.getElementById('password');
 const password2=document.getElementById('password2');
 
-// function showError(input, message){
-//     const formControl=input.parentElement;
-//     formControl.classname='form-control error';
 
-// }
+
+function checkemail(input){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    }else{
+        showError(input,`${getinput(input)} is not valid`);
+    }
+}
+
+function checkpass(input1,input2){
+    if (input1.value !==input2.value){
+        showError(input2, "Password does not match");
+
+    }
+}
+function checklength(input, min, max){
+    if(input.value.length<min){
+        showError(input,`${getinput(input)} is needs to minimum ${min} is required`);
+    }else if(input.value.length>max){
+        showError(input,`${getinput(input)} is needs to maximum ${max} is required`);
+
+    }else{
+        showSuccess(input);
+    }
+}
 
 function showError(input, message) {
    const formControl= input.parentElement;
@@ -24,35 +47,33 @@ function showSuccess(input, message) {
  
  }
 
+
+ // Function to check if required fields have data
+function checkRequired(inputArray) {
+    inputArray.forEach(function(input) {
+       if (input.value===''){
+       // console.log(input.id);
+        showError(input,`${getinput(input) } is required`)
+       }else{
+        showSuccess(input)
+       }
+    });
+}
+
+function getinput(input){
+    return input.id.charAt(0).toUpperCase()+input.id.slice(1);
+}
 //creat add event lisner for submit button
 //when submit event call than it call to fanction
 form.addEventListener('submit',function(e){
     //stop page from reloading
     e.preventDefault();
     //console.log(username.value);
-    if(username.value===''){
-       showError(username , 'user name is required ')
-    }else{
-        showSuccess(username);
-    }
-
-    if(email.value===''){
-        showError(email , 'user name is required ')
-     }else{
-         showSuccess(email);
-     }
-
-    if(password.value===''){
-        showError(password , 'user name is required ')
-     }else{
-         showSuccess(password);
-    }
-
-    if(password2.value===''){
-        showError(password2 , 'user name is required ')
-     }else{
-         showSuccess(password2);
-     }
+checkRequired([username,email,password,password2])
+checklength(username, 3, 10);
+checklength(password, 3, 10);
+checkemail(email);
+checkpass(password,password2);
 
 });
 
---
+++ project 2/script.js
@@ -0,0 +1,39 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occopied)')
+const count = document.getElementById('count');
+const total =document.getElementById('total');
+const movieSelect = document.getElementById('movie')
+
+let  ticketPrice= +movieSelect.value;
+
+function Updatecount(){
+    const selectedSeats= document.querySelectorAll('.row .seat.selected');
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    console.log(seatsIndex);
+    const selectedSeatscount= selectedSeats.length;
+    count.innerText=selectedSeatscount;
+    total.innerText= ticketPrice*selectedSeatscount;
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+
+
+   // console.log(total);
+   
+
+}
+localStorage.setItem('name','Adil');
+
+
+container.addEventListener('click', e => {
+    if(e.target.classList.contains('seat') && !(e.target.classList.contains('occupied')))
+    {
+        e.target.classList.toggle('selected');
+        Updatecount();
+    }
+})
+
+movieSelect.addEventListener('change',e =>{
+    ticketPrice= +e.target.value;
+    Updatecount();
+    
+})
